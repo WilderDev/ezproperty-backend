51593638eae1e5eaa081f53205865abf2423cb80
AlexMantzDev <alex.mantz@yahoo.com>
2024-04-03 13:47:45 -0500

    first commit worker actions

---
+++ controllers/worker.controller.js
@@ -0,0 +1,5 @@
+const addWorker = async (req, res) => {};
+const revokeWorker = async (req, res) => {};
+const getWorkerById = async (req, res) => {};
+const getAllWorkers = async (req, res) => {};
+const getWorkerByType = async (req, res) => {};
