028528c57947aadfcb30e6ab683e354d76e1f30b
Blake Graviett <blake.graviett@gmail.com>
2024-04-16 15:06:02 -0500

    emails send in block or completed

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -7,9 +7,7 @@ const {
 	updateTicket,
 	getTicket,
 	getAllTickets,
-	assignWorker,
-	blockedTicket,
-	completedTicket
+	assignWorker
 } = require("../controllers/ticket.controller");
 
 router.post("/create", authenticateUser, createTicket);
@@ -17,8 +15,6 @@ router.delete("/delete/:id", authenticateUser, deleteTicket);
 router.patch("/edit/:id", authenticateUser, updateTicket);
 router.get("/all", authenticateUser, getAllTickets);
 router.get("/find/:id", authenticateUser, getTicket);
-router.post("/edit/assign/:id", authenticateUser, assignWorker);
-router.post("/edit/blockage/:id", authenticateUser, blockedTicket);
-router.post("/edit/completed/:id", authenticateUser, completedTicket);
+router.patch("/edit/assign/:id", authenticateUser, assignWorker);
 
 module.exports = router;
